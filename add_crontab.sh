e368a3a03a42772203c6ae975a7a32737e48651a
TheusRM <70729901+TheusRM@users.noreply.github.com>
2023-11-08 14:13:15 -0300

    Update add_crontab.sh

--- add_crontab.sh
+++ add_crontab.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Verifique se o arquivo de script existe
-if [ -e /root/sataTelegrambot/bot.js ]; then
+if [ -e /root/penalty-street/bot.js ]; then
     # Adicione uma linha ao crontab
-    (crontab -l ; echo "@reboot /usr/bin/node /root/sataTelegrambot/startup-bot.sh") | crontab -
+    (crontab -l ; echo "@reboot /usr/bin/node /root/penalty-street/startup-bot.sh") | crontab -
     echo "Entrada adicionada ao crontab para iniciar /root/sataTelegrambot/startup-bot.sh na reinicialização."
 else
-    echo "O arquivo /root/sataTelegrambot/startup-bot.sh não foi encontrado."
+    echo "O arquivo /root/penalty-street/startup-bot.sh não foi encontrado."
 fi
